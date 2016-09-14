1b3be82ab314872530c3827884cd0ae3d190269a
Rüdiger Klaehn <rklaehn@gmail.com>
2016-09-14 14:45:00 +0100

    Allow ' for the end of identifiers
    So you can do things like val x' = 1 for versions of values

--- src/compiler/scala/tools/nsc/ast/parser/Scanners.scala
+++ src/compiler/scala/tools/nsc/ast/parser/Scanners.scala
@@ -628,7 +628,7 @@ trait Scanners extends ScannersCommon {
            'u' | 'v' | 'w' | 'x' | 'y' |
            'z' |
            '0' | '1' | '2' | '3' | '4' |
-           '5' | '6' | '7' | '8' | '9' =>
+           '5' | '6' | '7' | '8' | '9' | '\'' =>
         putChar(ch)
         nextChar()
         getIdentRest()
---
+++ test/files/pos/xprime.scala
@@ -0,0 +1,5 @@
+object Test {
+  val x = 0
+  val xy' = 0
+  val xyz'' = 0
+}
